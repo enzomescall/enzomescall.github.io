6aa4c93bc734fecc01182741567d10531f55bd55
Enzo Mescall <emm121@duke.edu>
2023-01-30 20:18:48 -0500

    added transitions

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ Array.from(document.getElementsByClassName("menu-item"))
       menu.dataset.activeIndex = index;
     }
   });
+
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 200)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
